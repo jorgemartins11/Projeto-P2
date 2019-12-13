64a33f992cdea7e101e3f429272cca0439305678
9180544@esmad.ipp.pt <9180544@esmad.ipp.pt>
2019-12-13 14:30:52 +0000

    home page updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,28 @@
 <template>
-  <div class="home">
-    <NavBar />
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+      <div class="imgContainer position-relative d-flex align-items-center justify-content-center">
+        <div class="barra position-absolute d-flex justify-content-center">
+          <div class="logo text-center">
+            <img src="../assets/logo.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-import NavBar from "@/components/NavBar.vue"
+<style scoped>
+  .imgContainer {
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld,
-    NavBar
+    background: url(../assets/background.jpg) no-repeat center center fixed; 
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
   }
-};
-</script>
+
+  .barra {
+    border-left: 400px solid #2C3E50;
+    height: 100%;
+    opacity: 0.75;
+    top: 0;
+  }
+</style>
