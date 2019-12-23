960fdbc6f349af635fb3a4fead98e74de7448438
9180544@esmad.ipp.pt <9180544@esmad.ipp.pt>
2019-12-23 22:56:59 +0000

    comentar codigo que deu erro

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -199,56 +199,59 @@ input:focus{
 
 <script src="../../account.js"></script>
 <script>
+  // registerVerification(form)
+  // checkPassword(str)
 
-  function registerVerification(form){
-      if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
-        if(form.pwd1.value.length < 8) {
-          alert("Error: Password must contain at least 8 characters!");
-          form.pwd1.focus();
-          return false;
-        }
-        if(form.pwd1.value == form.username.value) {
-          alert("Error: Password must be different from Username!");
-          form.pwd1.focus();
-          return false;
-        }
-        re = /[0-9]/;
-        if(!re.test(form.pwd1.value)) {
-          alert("Error: Password must contain at least one number (0-9)!");
-          form.pwd1.focus();
-          return false;
-        }
-        re = /[a-z]/;
-        if(!re.test(form.pwd1.value)) {
-          alert("Error: Password must contain at least one lowercase letter (a-z)!");
-          form.pwd1.focus();
-          return false;
-        }
-        re = /[A-Z]/;
-        if(!re.test(form.pwd1.value)) {
-          alert("Error: Password must contain at least one uppercase letter (A-Z)!");
-          form.pwd1.focus();
-          return false;
-        }
-    }
-    else if (form.pwd1.value == "" || form.pwd2.value == "") {
-       alert("Error: Please fill both Password fields and make sure their value is the same!");
-    }
-    else if (form.pwd1.value !== form.pwd2.value) {
-       alert("Error: Passwords do not match! Make sure they have the same value!");
-    }
-    else {
-      alert("Error: Please check that you've entered and confirmed your password!");
-      form.pwd1.focus();
-      return false;
-    }
-  };
+  // export function registerVerification(form){
+  //     //If password1 is not empty and if the password1 value matches password2 value    
+  //     if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
+  //       if(form.pwd1.value.length < 8) {
+  //         alert("Error: Password must contain at least 8 characters!");
+  //         form.pwd1.focus();
+  //         return false;
+  //       }
+  //       if(form.pwd1.value == form.username.value) {
+  //         alert("Error: Password must be different from Username!");
+  //         form.pwd1.focus();
+  //         return false;
+  //       }
+  //       re = /[0-9]/;
+  //       if(!re.test(form.pwd1.value)) {
+  //         alert("Error: Password must contain at least one number (0-9)!");
+  //         form.pwd1.focus();
+  //         return false;
+  //       }
+  //       re = /[a-z]/;
+  //       if(!re.test(form.pwd1.value)) {
+  //         alert("Error: Password must contain at least one lowercase letter (a-z)!");
+  //         form.pwd1.focus();
+  //         return false;
+  //       }
+  //       re = /[A-Z]/;
+  //       if(!re.test(form.pwd1.value)) {
+  //         alert("Error: Password must contain at least one uppercase letter (A-Z)!");
+  //         form.pwd1.focus();
+  //         return false;
+  //       }
+  //   }
+  //   else if (form.pwd1.value == "" || form.pwd2.value == "") {
+  //      alert("Error: Please fill both Password fields and make sure their value is the same!");
+  //   }
+  //   else if (form.pwd1.value !== form.pwd2.value) {
+  //      alert("Error: Passwords do not match! Make sure they have the same value!");
+  //   }
+  //   else {
+  //     alert("Error: Please check that you've entered and confirmed your password!");
+  //     form.pwd1.focus();
+  //     return false;
+  //   }
+  // }
 
-  function checkPassword(str)
-  {
-    // at least one number, one lowercase and one uppercase letter
-    // at least 8 characters
-    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-    return re.test(str);
-  }
+  // export function checkPassword(string)
+  // {
+  //   // Have at least one number, one lowercase and one uppercase letter
+  //   // Have at least 8 characters
+  //   var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+  //   return re.test(string);
+  // }
   </script>
