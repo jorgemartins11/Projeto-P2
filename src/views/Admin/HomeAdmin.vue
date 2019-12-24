28cc9c9845d747a4a4fd22dfdfc06e8adc8af200
9180544@esmad.ipp.pt <9180544@esmad.ipp.pt>
2019-12-24 19:05:42 +0000

    commit

--- src/views/Admin/HomeAdmin.vue
+++ src/views/Admin/HomeAdmin.vue
@@ -1,66 +1,148 @@
 <template>
   <div class="home">
-    <AdminNavBar />
+    <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
+      <a class="navbar-brand pl-5" href="#">
+        <router-link to="/home">
+          <img src="../../assets/logo.png" />
+        </router-link>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto text-center">
+        </ul>
+        <router-link to="/profile">
+          <img
+            src="../../assets/profile_icon.png"
+            class="mr-lg-5 pl-sm-5 pl-lg-0"
+            width="30px"
+            height="30px"
+          />
+        </router-link>
+      </div>
+    </nav>
     <div class="imgContainer position-relative d-flex align-items-center justify-content-center">
-        <div class="btn1 mr-lg-5 pl-sm-5 pl-lg-0">
-            <router-link to="/reservationsAdmin">
-            <img src="../../assets/reservas.png" />
-            <hr>
-            <p class="p">Reservas</p>
-            <br />
-            </router-link>
-        </div>
-        <div class="btn2 mr-lg-5 pl-sm-5 pl-lg-0">
-            <router-link to="/usersAdmin">
-            <img src="../../assets/users.png" />
-            <hr>
-            <p class="p">Utilizadores</p>
-            <br />
-            </router-link>
-        </div>
-        <div class="btn3 mr-lg-5 pl-sm-5 pl-lg-0">
-            <router-link to="/stockAdmin">
-            <img src="../../assets/stock.png" />
-            <hr>
-            <p class="p">Gestão de Stock</p>
-            <br />
-            </router-link>
-        </div>
+      <div class="btn1 mr-lg-5 pl-sm-5 pl-lg-0">
+        <router-link to="/reservationsAdmin">
+          <img src="../../assets/reservas.png" />
+          <hr />
+          <p class="p">Reservas</p>
+          <br />
+        </router-link>
+      </div>
+      <div class="btn2 mr-lg-5 pl-sm-5 pl-lg-0">
+        <router-link to="/usersAdmin">
+          <img src="../../assets/users.png" />
+          <hr />
+          <p class="p">Utilizadores</p>
+          <br />
+        </router-link>
+      </div>
+      <div class="btn3 mr-lg-5 pl-sm-5 pl-lg-0">
+        <router-link to="/stockAdmin">
+          <img src="../../assets/stock.png" />
+          <hr />
+          <p class="p">Gestão de Stock</p>
+          <br />
+        </router-link>
+      </div>
     </div>
-    <AdminFooter />
+    <div class="container-fluid Footer-custom fixed-bottom pb-3 pt-3">
+      <div class="row row-no-gutters" id="bottom-footer">
+        <div class="col-xs-12 col-md-5 col-lg-4 text-center">
+          <ul class="vertical-aligned small pt-5 mr-5">
+            <a href="https://www.ipp.pt/" class="mr-5">
+              <img src="../../assets/ipplogo-white.svg" />
+            </a>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-md-2 col-lg-4 text-center">
+          <ul class="list-unstyled list-inline pt-2">
+            <a
+              id="copyright"
+              class="list-inline-item"
+            >&copy; André Lopes, Gonçalo Moreira e Jorge Martins</a>
+          </ul>
+        </div>
+        <div class="col-xs-12 col-md-5 col-lg-4 text-center pt-5">
+          <a href="https://www.facebook.com/pporto.esht/" class="pl-2">
+            <img src="../../assets/facebook.svg" />
+          </a>
+          <a href="https://twitter.com/politecnico" class="pl-2">
+            <img src="../../assets/twitter.svg" />
+          </a>
+          <a href="https://www.instagram.com/esht_pporto/" class="pl-2">
+            <img src="../../assets/instagram.svg" />
+          </a>
+          <a href="https://www.youtube.com/user/politecnicodoporto" class="pl-2">
+            <img src="../../assets/youtube.svg" />
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import AdminFooter from "@/components/AdminFooter.vue";
-import AdminNavBar from "@/components/AdminNavBar.vue";
-
 export default {
   name: "about",
-  components: {
-    AdminFooter,
-    AdminNavBar
-  }
+  components: {}
 };
 </script>
 
 <style scoped>
-.container{
-    margin-left: auto;
-    margin-right: auto;
-    width: 1000px;
-    position: absolute;
+@import url("https://fonts.googleapis.com/css?family=Muli&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Muli&display=swap");
+
+.Footer-custom {
+  background-color: #2c3e50;
+  font-family: "Muli", sans-serif;
+  font-size: 1rem;
 }
 
-.btn1, .btn2, .btn3 {
-    width: 600px;
-    height: 600px;
-    float:left;
-    background-color: #707070;
-    margin-top: 125px;
-    margin-left: 40px;
-    padding: 10px;
-    border: 10px solid #2c3e50;
+.navbar-custom {
+  background-color: #2c3e50;
+  height: 80px;
+  font-family: "Muli", sans-serif;
+  font-size: 1rem;
+}
+
+.navbar-brand,
+.nav-link {
+  color: #ffffff;
+}
+
+.collapse {
+  background-color: #2c3e50;
+}
+
+.container {
+  margin-left: auto;
+  margin-right: auto;
+  width: 1000px;
+  position: absolute;
+}
+
+.btn1,
+.btn2,
+.btn3 {
+  width: 600px;
+  height: 600px;
+  float: left;
+  background-color: #707070;
+  margin-top: 125px;
+  margin-left: 40px;
+  padding: 10px;
+  border: 10px solid #2c3e50;
 }
 
 a {
@@ -72,63 +154,16 @@ a {
 a:hover {
   text-decoration: none !important ;
   color: #2c3e50 !important;
-  font-size: 20px;
+  font-weight: bolder;
 }
 
-#welcome {
-  font-size: 30px;
+#copyright:hover{
+  text-decoration: none !important ;
+  color: white !important;
+  font-weight:normal !important;
 }
 
-.barra {
-  background-color: white;
-  height: 100%;
-  width: 30rem;
-  top: 0;
-  position: absolute;
-}
-
-img#logo {
-  height: auto;
-  width: 30%;
-}
-
-img#bg {
-  position: fixed;
-  height: 100%;
-  width: 100%;
-}
-
-.img-fluid {
-  max-width: 100%;
-  height: auto;
-}
-
-hr {
-  height: 1px;
-  width: 20rem;
-  background-color: #2c3e50;
-}
-
-small {
-  font-family: "Muli", sans-serif;
-  font-size: 0.9rem;
-  font-weight: bold;
-  color: #2c3e50;
-  opacity: 1;
-}
-
-.form-control {
-  background-color: #707070;
-  border-color: #707070;
-}
-
-input:focus {
-  background-color: #707070;
-  border: 1px solid black !important;
-  box-shadow: unset;
-}
-
-.welcomeText {
-  color: #2c3e50;
+#copyright{
+ margin-top: 40px;
 }
 </style>
